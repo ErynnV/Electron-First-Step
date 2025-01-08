42be62b9d15ae166372b24463410d942e051daf9
Erynn <vandreerynn@gmail.com>
2025-01-08 15:49:22 +0100

    first use of Fils system

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, ipcMain } = require("electron");
 const path = require('path')
+const fs = require('fs')
 
 let mainWindow;
 
@@ -25,11 +26,44 @@ function createWindow () {
 
     mainWindow.setMenuBarVisibility(false);
 
-    //mainWindow.loadURL("https://gamenium.fr ")
+    //mainWindow.loadURL("https://neal.fun/stimulation-clicker/")
     mainWindow.loadFile('front/main.html')
 
 }
 
 app.whenReady().then(() => {
     createWindow();
+})
+
+ipcMain.on('hello', () => {
+
+    console.log("hello i'm back-end");
+
+})
+
+
+let myPass = 'JeSuisUnPass';
+
+ipcMain.on('getMyVariable', (event, username) => {
+
+    console.log('Demande prise en compte pour ' + username)
+
+    event.sender.send('HavePass', myPass)
+
+    if (!fs.existsSync(path.join(app.getPath('appData') + '/.myFirstApp'))){
+
+        fs.mkdir(path.join(app.getPath('appData') + '/.myFirstApp'), (err) =>{
+            if (err) throw err;
+        })
+
+    }
+
+    fs.appendFile(path.join(app.getPath('appData') + '/.myFirstApp/pass.txt'), myPass, (err) => {
+        if (err) throw err;
+    })
+
+    fs.readFile(path.join(app.getPath('appData') + '/.myFirstApp/pass.txt'), 'utf8', (err, data) => {
+        if (err) throw err;
+        console.log('data = ' + data)
+    })
 })
